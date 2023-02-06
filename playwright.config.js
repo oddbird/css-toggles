1ae1a30759609a0adca9b4e555ef585e1a9fee98
Jonny Gerig Meyer <jonny@oddbird.net>
2023-02-06 11:52:27 -0500

    increase timeouts

--- playwright.config.js
+++ playwright.config.js
@@ -1,16 +1,16 @@
 export default {
-  timeout: process.env.CI ? undefined : 3000, // Max execution time of any single test
+  timeout: process.env.CI ? undefined : 10 * 1000, // Max execution time of any single test
   expect: {
     timeout: 1000, // Max execution time of single expect() calls
   },
   webServer: {
     command: 'PORT=4000 LEVEL=warning yarn preview',
-    url: 'http://localhost:4000/',
-    timeout: 10 * 1000,
+    port: 4000,
+    timeout: process.env.CI ? 60 * 1000 : 10 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: 'http://localhost:4000',
+    baseURL: 'http://localhost:4000/',
     browserName: 'chromium',
     headless: true,
     forbidOnly: process.env.CI,
